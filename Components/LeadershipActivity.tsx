7d764c369d0bdcb55ff8ea1a67a0f4ab099cbd37
Jemishhh <jemishpatel4545@gmail.com>
2024-01-04 14:32:20 +0530

    'A'

--- Components/LeadershipActivity.tsx
+++ Components/LeadershipActivity.tsx
@@ -15,7 +15,7 @@ const LeadershipActivity = ({name,role,image,para}:Props) => {
   return (
     <div className='flex flex-col text-center justify-center'>
 
-    <Image src={image} alt={name} width={100} height={100} objectFit="contain" className='mx-auto mb-[2rem] rounded-full' fill/>      
+    <Image src={image} alt={name} width={100} height={100} objectFit="contain" className='mx-auto mb-[2rem] rounded-full'/>      
         <div className='flex items-center mx-auto'>
             <StarIcon className='w-[2rem] h-[2rem] text-yellow-400'/>
             <StarIcon className='w-[2rem] h-[2rem] text-yellow-400'/>
